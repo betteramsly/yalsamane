a4904dbd3f66f1fd97147837d8ebd0abcd13f56a
Islam Shagaev <islamshg@mail.ru>
2023-07-29 19:15:02 +0300

    feat: changed folder structure to next older version

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import type { AppProps } from 'next/app'
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div>
+      <title>Муса / Ялсамане</title>
+      <meta
+        name="viewport"
+        content="minimum-scale=1, initial-scale=1, width=device-width"
+      />
+      <Component {...pageProps} />
+    </div>
+  )
+}
